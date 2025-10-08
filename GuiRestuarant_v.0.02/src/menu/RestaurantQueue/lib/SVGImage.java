8789ae67582de91b4a3dcd11ece2507c0e140e7f
Trinn <trinnaphat.c@ku.th>
2025-10-08 19:57:10 +0700

    Add files via upload

---
+++ GuiRestuarant_v.0.02/src/menu/RestaurantQueue/lib/SVGImage.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ *//*
+package lib;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import javax.swing.*;
+
+/**
+ *
+ * @author ACER
+ *//*
+public class SVGImage extends JLabel {
+    private FlatSVGIcon svgIcon;
+    
+    public void SVGImage(String image, int width, int height){
+    svgIcon = new FlatSVGIcon(image, width, height);
+    setIcon(svgIcon);
+    }*/
+//}
